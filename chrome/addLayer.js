f44dc3bb4b6711912abdc20d601c16f2cec5c768
mahengyang <hengyangma@gmail.com>
2013-11-02 23:12:25 +0800

    more pretty code

--- chrome/addLayer.js
+++ chrome/addLayer.js
@@ -2,17 +2,18 @@
 $('#hplogo').children().text("enyo");
 
 var user_info = "http://angryang.com/cgi-bin/ruby/user_info.rb";
+// get user info from server
 $.get(user_info,function(data,status){
 	console.log("response from server "+ data)
-
-	newdiv = "<div class='linkweb'></div>";
-	$(document.body).append(newdiv)
-	$('.linkweb').append("<ul>")
 	user_info = data.enyo
+
+	ul = $("<ul></ul>")
 	for(i in user_info){
 		console.log(user_info[i])
-		$('.linkweb').append("<li><a href=\"" + user_info[i].url + "\"></a>" + user_info[i].name + "</li>")
+		
+		li = $("<li></li>")
+		link = $("<a></a>").text(user_info[i].name).attr("href",user_info[i].url)
+		li.prepend(link).prependTo(ul)
 	}
-
-	$('.linkweb').append("</ul>")
+	$("<div class='linkweb'></div>").prepend(ul).prependTo(document.body);
   });
