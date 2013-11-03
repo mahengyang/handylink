4e066fc52a8cadb893aa9ad5a97c9c79f3aab5bc
mahengyang <hengyangma@gmail.com>
2013-11-03 15:24:52 +0800

    read url from mysql

--- chrome/js/addLayer.js
+++ chrome/js/addLayer.js
@@ -1,17 +1,16 @@
 ﻿console.log('this is from addLayer.js');
 $('#hplogo').children().text("enyo");
 
-var user_info = "http://angryang.com/cgi-bin/ruby/user_info.rb";
+var url = "http://angryang.com/cgi-bin/ruby/user_info.rb";
 // get user info from server
-$.get(user_info,function(data,status){
+$.get(url,function(data,status){
 	console.log("response from server "+ data)
-	user_info = data.enyo
 
 	ul = $("<ul></ul>")
-	for(i in user_info){
-		console.log(user_info[i])
+	for(i in data){
+		console.log(data[i])
 		
-		link = $("<a></a>").text(user_info[i].name).attr("href",user_info[i].url)
+		link = $("<a></a>").text(data[i].title).attr("href",data[i].url)
 		$("<li></li>").prepend(link).prependTo(ul)
 	}
 	$("<div class='linkweb'></div>").prepend(ul).prependTo(document.body);
