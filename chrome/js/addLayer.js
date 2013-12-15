73980ce0cd6d811c35b47a0a75ea1ba1de39965b
mahengyang <hengyangma@gmail.com>
2013-12-15 13:49:55 +0800

    css change

--- chrome/js/addLayer.js
+++ chrome/js/addLayer.js
@@ -1,13 +1,14 @@
 ﻿console.log('this is from addLayer.js');
 
-var url = "http://angryang.com/cgi-bin/ruby/user_info.rb";
+var url = "http://angryang.com/cgi-bin/ruby/user_info.rb?handle=get";
 // get user info from server
 $.get(url,function(data,status){
-	console.log("response from server "+ data)
+	console.log("response from server")
 
 	ul = $("<ul></ul>")
 	for(i in data){
-		console.log(data[i])
+		console.log("-----title--"+data[i].title)
+		console.log("-----url--"+data[i].url)
 		
 		link = $("<a></a>").text(data[i].title).attr("href",data[i].url)
 		$("<li></li>").prepend(link).prependTo(ul)
